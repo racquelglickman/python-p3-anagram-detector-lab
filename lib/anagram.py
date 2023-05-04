ac441c10d9574c0e68d23f4acc07d1beee46c4e1
Racquel Glickman <racquel.glickman@gmail.com>
2023-05-04 11:36:10 -0400

    completes lab

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,8 @@
 # your code goes here!
+class Anagram:
+    
+    def __init__(self, word):
+        self.word = word
+
+    def match(self, anagrams):
+        return [anagram for anagram in anagrams if sorted(anagram) == sorted(self.word)]
